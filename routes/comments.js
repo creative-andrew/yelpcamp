4301949c316f905ea85a3a327b0989da880e1970
geist.ahnen <geist.ahnen@gmail.com>
2017-03-20 21:04:50 -0500

    comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,12 +41,76 @@ router.post("/", function(req, res) {
   });
 });
 
+router.get("/:comment_id/edit",mirarAutorizacion2, function (req, res){
+var campground_id = req.params.id;
+     Comment.findById(req.params.comment_id, function(err,comment) {
+        if (err) {
+          console.log(err)
+        }
+        else {
+          res.render('comments/edit', {campground_id: campground_id, comment: comment });
+        }
+  });
+   
+}); 
+
+
+router.post("/:comment_id",mirarAutorizacion2, function (req, res) {
+Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function (err, updatedComment){
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log(updatedComment)
+    res.redirect("/campgrounds/"+req.params.id)
+  }
+})
+
+});
+
+
+router.delete('/:comment_id',mirarAutorizacion2, function (req, res){
+Comment.findByIdAndRemove(req.params.comment_id, function(err){
+      if (err) {
+        console.log(err)
+      }
+      else {
+        res.redirect('/campgrounds/'+req.params.id);
+      }
+});
+});
+
+
+
+
 function isLoggedIn(req, res, next){
   if (req.isAuthenticated()){
       return next();
   };
   res.redirect('/login');
 };
+
+function mirarAutorizacion2(req, res, next) {
+  if (req.isAuthenticated()) {
+      Comment.findById(req.params.comment_id, function(err, comment){
+          if (err) {
+            console.log(err)
+          }
+          else {
+           if (comment.author.id.equals(req.user._id)){
+                next();
+          }
+          else {
+            res.redirect('back');
+          }
+           };
+      });
+  }
+  else {
+    res.redirect('back');
+  }
+};
+
 
 
 module.exports = router;
